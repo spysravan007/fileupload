80514c20da9f30d80d2ec12227ada9cc13ee6588
Your Name <you@example.com>
2021-11-14 14:56:54 +0000

    files

---
+++ FileUpload/FileData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileUpload
+{
+    public class FileData
+    {
+        private string _FileName;
+        public string FileName
+        {
+            get { return this._FileName; }
+            set { this._FileName = value; }
+        }
+
+        private System.Windows.Media.Imaging.BitmapImage _ImageData;
+        public System.Windows.Media.Imaging.BitmapImage ImageData
+        {
+            get { return this._ImageData; }
+            set { this._ImageData = value; }
+        }
+
+    }
+}
--- FileUpload/MainWindow.xaml.cs
+++ FileUpload/MainWindow.xaml.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -19,103 +24,96 @@ using System.Windows.Shapes;
 
 namespace FileUpload
 {
-
-    internal class ProgressableStreamContent : HttpContent
-    {
-        /// <summary>
-        /// Lets keep buffer of 20kb
-        /// </summary>
-        private const int defaultBufferSize = 5 * 4096;
-
-        private HttpContent content;
-
-        private int bufferSize;
-
-        //private bool contentConsumed;
-        private Action<long, long> progress;
-
-        public ProgressableStreamContent(HttpContent content, Action<long, long> progress) : this(content,
-            defaultBufferSize, progress)
-        {
-        }
-
-        public ProgressableStreamContent(HttpContent content, int bufferSize, Action<long, long> progress)
-        {
-            if (content == null)
-            {
-                throw new ArgumentNullException("content");
-            }
-
-            if (bufferSize <= 0)
-            {
-                throw new ArgumentOutOfRangeException("bufferSize");
-            }
-
-            this.content = content;
-            this.bufferSize = bufferSize;
-            this.progress = progress;
-
-            foreach (var h in content.Headers)
-            {
-                this.Headers.Add(h.Key, h.Value);
-            }
-        }
-
-        protected override Task SerializeToStreamAsync(Stream stream, TransportContext context)
-        {
-            return Task.Run(async () =>
-            {
-                var buffer = new Byte[this.bufferSize];
-                long size;
-                TryComputeLength(out size);
-                var uploaded = 0;
-
-
-                using (var sinput = await content.ReadAsStreamAsync())
-                {
-                    while (true)
-                    {
-                        var length = sinput.Read(buffer, 0, buffer.Length);
-                        if (length <= 0) break;
-
-                        //downloader.Uploaded = uploaded += length;
-                        uploaded += length;
-                        progress?.Invoke(uploaded, size);
-
-                        //System.Diagnostics.Debug.WriteLine($"Bytes sent {uploaded} of {size}");
-
-                        stream.Write(buffer, 0, length);
-                        stream.Flush();
-                    }
-                }
-
-                stream.Flush();
-            });
-        }
-
-        protected override bool TryComputeLength(out long length)
-        {
-            length = content.Headers.ContentLength.GetValueOrDefault();
-            return true;
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                content.Dispose();
-            }
-
-            base.Dispose(disposing);
-        }
-    }
-
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
             GetFiles();
+        }
+
+        private void AddFileToView(String file_id, String file_name, String file_desc, String file_path)
+        {
+            Trace.WriteLine(file_name);
+            //FileViewer.Children.Add(new TextBlock() { Text = file_name });
+            //var icon = System.Drawing.Icon.ExtractAssociatedIcon(file_name);
+            //var bmp = icon.ToBitmap();
+            var bmp = GetFileIcon(file_name).ToBitmap();
+            this.FileViewer.Items.Add(new FileData { FileName = file_name, ImageData = ToBitmapImage(bmp) });
+            //icon = System.Windows.Interop.Imaging.CreateBitmapSourceFromHIcon(
+            //        sysicon.Handle,
+            //        System.Windows.Int32Rect.Empty,
+            //        System.Windows.Media.Imaging.BitmapSizeOptions.FromEmptyOptions())
+
+        }
+
+        static class Shell32
+        {
+            private const int MaxPath = 256;
+            [StructLayout(LayoutKind.Sequential)]
+            public struct Shfileinfo
+            {
+                private const int Namesize = 80;
+                public readonly IntPtr hIcon;
+                private readonly int iIcon;
+                private readonly uint dwAttributes;
+                [MarshalAs(UnmanagedType.ByValTStr, SizeConst = MaxPath)]
+                private readonly string szDisplayName;
+                [MarshalAs(UnmanagedType.ByValTStr, SizeConst = Namesize)]
+                private readonly string szTypeName;
+            };
+            public const uint ShgfiIcon = 0x000000100;     // get icon
+            public const uint ShgfiLinkoverlay = 0x000008000;     // put a link overlay on icon
+            public const uint ShgfiLargeicon = 0x000000000;     // get large icon
+            public const uint ShgfiSmallicon = 0x000000001;     // get small icon
+            public const uint ShgfiUsefileattributes = 0x000000010;     // use passed dwFileAttribute
+            public const uint FileAttributeNormal = 0x00000080;
+            [DllImport("Shell32.dll")]
+            public static extern IntPtr SHGetFileInfo(
+                string pszPath,
+                uint dwFileAttributes,
+                ref Shfileinfo psfi,
+                uint cbFileInfo,
+                uint uFlags
+                );
+        }
+        static class User32
+        {
+            /// <summary>
+            /// Provides access to function required to delete handle. This method is used internally
+            /// and is not required to be called separately.
+            /// </summary>
+            /// <param name="hIcon">Pointer to icon handle.</param>
+            /// <returns>N/A</returns>
+            [DllImport("User32.dll")]
+            public static extern int DestroyIcon(IntPtr hIcon);
+        }
+        public static System.Drawing.Icon GetFileIcon(string name)
+        {
+            Shell32.Shfileinfo shfi = new Shell32.Shfileinfo();
+            uint flags = Shell32.ShgfiIcon | Shell32.ShgfiUsefileattributes;
+            flags += Shell32.ShgfiLinkoverlay;
+            flags += Shell32.ShgfiLargeicon;
+            Shell32.SHGetFileInfo(name, Shell32.FileAttributeNormal, ref shfi, (uint)System.Runtime.InteropServices.Marshal.SizeOf(shfi), flags);
+            System.Drawing.Icon icon = (System.Drawing.Icon) System.Drawing.Icon.FromHandle(shfi.hIcon).Clone();
+            User32.DestroyIcon(shfi.hIcon);
+            return icon;
+        }
+
+        public static BitmapImage ToBitmapImage(Bitmap bitmap)
+        {
+            using (var memory = new MemoryStream())
+            {
+                bitmap.Save(memory, ImageFormat.Png);
+                memory.Position = 0;
+                var bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+                bitmapImage.Freeze();
+                return bitmapImage;
+            }
         }
 
         private async void GetFiles()
@@ -126,7 +124,13 @@ namespace FileUpload
             {
                 if (response.IsSuccessStatusCode)
                 {
-                    Trace.WriteLine(response.Content.ReadAsStringAsync().Result);
+                    var objects = JArray.Parse(response.Content.ReadAsStringAsync().Result);
+                    JArray array = JArray.Parse(response.Content.ReadAsStringAsync().Result);
+                    foreach (JObject obj in array.Children<JObject>())
+                    {
+                        Trace.WriteLine(obj.GetValue("file_name"));
+                        AddFileToView(obj.GetValue("file_id").ToString(), obj.GetValue("file_name").ToString(), obj.GetValue("file_desc").ToString(), obj.GetValue("file_path").ToString());
+                    }
                 }
             }
         }
@@ -151,7 +155,7 @@ namespace FileUpload
                 var uploaded_file = new ProgressableStreamContent(new StreamContent(File.OpenRead(filePath)), (sent, total) => {
                     Trace.WriteLine("Uploading " + ((float)sent / total) * 100f);
                 });
-                multiPartStream.Add(new StringContent("file_name"), "file_name");
+                multiPartStream.Add(new StringContent(System.IO.Path.GetFileName(filePath)), "file_name");
                 multiPartStream.Add(new StringContent("file_desc"), "file_desc");
                 multiPartStream.Add(uploaded_file, "uploaded_file", System.IO.Path.GetFileName(filePath));
                 HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, uploadURL);
